babef15c63b33b97204031ed9b79cad6afe76036
Hunter Westerlund <hunterw.mail@gmail.com>
2024-12-03 11:23:21 -0700

    feat: day 3 part 2

--- Day-3/src/main.rs
+++ Day-3/src/main.rs
@@ -1,12 +1,11 @@
+use regex::{Match, Regex};
 use std::{fs::File, io::Read, path::Path};
-use regex::Regex;
 
 fn main() {
     println!("Day 3");
 
     let mut mul_total: i32 = 0i32;
-    let regex: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-
+    let mul_regex: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     // Create a path to the desired file
     let path = Path::new("./data.txt");
     let display = path.display();
@@ -26,11 +25,78 @@ fn main() {
     println!("Is Ascii? {}", s.is_ascii());
 
     // Find and capture each instance of the regex
-    for (full, [first, second]) in regex.captures_iter(&s).map(|c| c.extract()) {
+    for (full, [first, second]) in mul_regex.captures_iter(&s).map(|c| c.extract()) {
         //println!("Found match: {} with numbers {} and {}", full, first, second);
         mul_total += first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap();
     }
 
-    println!("Total: {}", mul_total)
+    println!("Total: {}", mul_total);
 
+    println!("Enabled Total: {}", enabled_total(&s));
+}
+
+fn enabled_total(instructions: &String) -> i32 {
+    let mul_regex: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let do_regex: Regex = Regex::new(r"do(n't)?\(\)").unwrap();
+    let mut is_do = true; // start enabled
+    let mut total = 0i32;
+    let mut pos = 0;
+
+    while pos < instructions.len() {
+        let mul_match = mul_regex.find_at(&instructions, pos);
+        let do_match = do_regex.find_at(&instructions, pos);
+        //println!("Do: {} | Total: {} | Position: {}", is_do, total, pos);
+        match (mul_match, do_match) {
+            (Some(mm), Some(dm)) => {
+                if mm.start() < dm.start() {
+                    if is_do {
+                        //println!("Found mul at {}", mm.start());
+                        total += handle_mul(&mm);
+                    }
+                    pos = mm.end();
+                } else {
+                    //println!("Found do at {}", dm.start());
+                    is_do = handle_do(&dm);
+                    pos = dm.end();
+                }
+            }
+            (Some(mm), None) => {
+                if is_do {
+                    //println!("Found mul at {}", mm.start());
+                    total += handle_mul(&mm);
+                }
+                pos = mm.end();
+            }
+            (None, Some(dm)) => {
+                //println!("Found do at {}", dm.start());
+                is_do = handle_do(&dm);
+                pos = dm.end();
+            }
+            (None, None) => break, // There are no more relevant instructions
+        }
+    }
+    return total;
+}
+
+fn handle_mul(mat: &Match) -> i32 {
+    let mul_regex: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let Some((full, [first, second])) = mul_regex.captures(mat.as_str()).map(|c| c.extract())
+    else {
+        return 0;
+    };
+    first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap()
+}
+
+fn handle_do(mat: &Match) -> bool {
+    let do_regex: Regex = Regex::new(r"do(n't)?\(\)").unwrap();
+    if let Some(caps) = do_regex.captures(mat.as_str()) {
+        let dont = caps.get(1).map(|m| m.as_str());
+
+        match dont {
+            Some(_) => false, // has "n't"
+            None => true,     // just "do()"
+        }
+    } else {
+        false // no match at all
+    }
 }
